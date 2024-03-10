808dd133400a5bd2132c86dfbe19837002af9f5f
MarcosVinicius-Mota <marcosvinicius.mota652790@gmail.com>
2024-03-10 13:57:41 -0300

    Nossa

--- Projeto/parser/parser_test.c
+++ Projeto/parser/parser_test.c
@@ -4,8 +4,6 @@ void funcao(int[][1]);
 void funcao2(void[][1]);
 int funcao3(int);
 
-
-
 int main()
 {
     int xor_assign = num |= 2;
@@ -13,6 +11,15 @@ int main()
     int error = num |= 2 &= 1;
     int n = 2 &= 1 |= 1;
 
+    int n = (nossa(12, 1));
+    n[0][0] = 10;
+
+    *ptr = 10;
+    int * a = &n;
+
+    int result = vetor_de_funcao_que_retorna_ponteiro[0][1](param1, param2)[0][0];
+    auto static unsigned int a;
+    i < 10;
 }
 
 
--- Projeto/parser/sintatic.py
+++ Projeto/parser/sintatic.py
@@ -4,10 +4,31 @@ import ply.yacc
 
 def p_program(p):
     """
+        program :
+    """
+
+
+def p_program_function_signature(p):
+    """
         program : function_signature SEMICOLON program
-                | variable_declaration SEMICOLON program
-                | function program
-                |
+    """
+
+
+def p_program_variable_declaration(p):
+    """
+        program : variable_declaration SEMICOLON program
+    """
+
+
+def p_program_function(p):
+    """
+        program : function program
+    """
+
+
+def p_program_assign(p):
+    """
+        program : expression program
     """
 
 
@@ -29,7 +50,6 @@ def p_command(p):
     """
         command : variable_declaration SEMICOLON
                 | variable_declaration ASSIGN expression SEMICOLON
-                | IDENTIFIER ASSIGN expression SEMICOLON
                 | expression SEMICOLON
                 | SEMICOLON
     """
@@ -112,14 +132,28 @@ def p_pointer(p):
 def p_type(p):
     """
         type : user_types
-            | primitive_types
+             | primitive_types
+             | type_modifier type
+    """
+
+
+# Completar os modificadores de tipo ex: static int | unsingned long long int | short int
+def p_type_modifier(p):
+    """
+        type_modifier : KEYWORD_STATIC
+                        | KEYWORD_UNSIGNED
+                        | KEYWORD_VOLATILE
+                        | KEYWORD_EXTERN
+                        | KEYWORD_SIGNED
+                        | KEYWORD_REGISTER
+                        | KEYWORD_AUTO
     """
 
 
 def p_user_types(p):
     """
-        user_types : IDENTIFIER
-                    | KEYWORD_STRUCT IDENTIFIER
+        user_types : KEYWORD_STRUCT IDENTIFIER
+                   | KEYWORD_UNION IDENTIFIER
     """
 
 
@@ -153,35 +187,271 @@ def p_integer_number(p):
 
 def p_expression(p):
     """
-        expression : and_assign_bitwise_operator
+        expression : assign_exp
     """
 
 
-def p_and_assign_bitwise_operator(p):
+def p_assign_operator(p):
     """
-        and_assign_bitwise_operator : xor_assign_bitwise_operator BITWISE_AND_ASSIGN and_assign_bitwise_operator
-                                    | xor_assign_bitwise_operator
-    """
-
-
-def p_xor_assign_bitwise_operator(p):
-    """
-        xor_assign_bitwise_operator : or_assign_bitwise_operator BITWISE_XOR_ASSIGN xor_assign_bitwise_operator
-                                    | or_assign_bitwise_operator
-    """
-
-
-def p_or_assign_bitwise_operator(p):
-    """
-        or_assign_bitwise_operator : or_assign_bitwise_operator BITWISE_OR_ASSIGN left_shift_bitwise_assign
-                                    | left_shift_bitwise_assign
+        assign_operator : ASSIGN
+                        | TIMES_ASSIGN
+                        | DIVIDE_ASSIGN
+                        | MODULUS_ASSIGN
+                        | PLUS_ASSIGN
+                        | MINUS_ASSIGN
+                        | SHIFT_LEFT_ASSIGN
+                        | SHIFT_RIGHT_ASSIGN
+                        | BITWISE_AND_ASSIGN
+                        | BITWISE_XOR_ASSIGN
+                        | BITWISE_OR_ASSIGN
     """
 
 
-def p_left_shift_bitwise_assign(p):
+def p_assign_exp(p):
     """
-        left_shift_bitwise_assign : IDENTIFIER
-                                  | integer_number
+        assign_exp : unary_exp assign_operator assign_exp
+                    | ternary_conditional_exp
+    """
+
+
+def p_ternary_conditional_exp(p):
+    """
+        ternary_conditional_exp : logical_exp QUESTION_MARK expression COLON ternary_conditional_exp
+                                | logical_exp
+    """
+
+
+def p_logical_exp(p):
+    """
+        logical_exp : logical_or_exp
+    """
+
+
+def p_logical_or_exp(p):
+    """
+        logical_or_exp : logical_or_exp LOGICAL_OR logical_and_exp
+                        | logical_and_exp
+    """
+
+
+def p_logical_and_exp(p):
+    """
+        logical_and_exp : logical_and_exp LOGICAL_AND bitwise_or_exp
+                        | bitwise_or_exp
+    """
+
+
+def p_bitwise_or_exp(p):
+    """
+        bitwise_or_exp : bitwise_or_exp BITWISE_OR bitwise_xor_exp
+                        | bitwise_xor_exp
+    """
+
+
+def p_bitwise_xor_exp(p):
+    """
+        bitwise_xor_exp : bitwise_xor_exp BITWISE_XOR bitwise_and_exp
+                        | bitwise_and_exp
+    """
+
+
+def p_bitwise_and_exp(p):
+    """
+        bitwise_and_exp : bitwise_and_exp BITWISE_AND is_equals_exp
+                        | is_equals_exp
+    """
+
+
+def p_is_equals_exp(p):
+    """
+        is_equals_exp : is_equals_exp EQUALS_THEN is_not_equals_exp
+                       | is_not_equals_exp
+    """
+
+
+def p_is_not_equals(p):
+    """
+        is_not_equals_exp : is_not_equals_exp NOT_EQUALS greater_then_exp
+                          | greater_then_exp
+    """
+
+
+def p_greater_then_exp(p):
+    """
+        greater_then_exp : greater_then_exp GREATER_THEN greater_equals_exp
+                         | greater_equals_exp
+    """
+
+
+def p_greater_equals_exp(p):
+    """
+         greater_equals_exp :  greater_equals_exp GREATER_EQUALS less_then_exp
+                            | less_then_exp
+    """
+
+
+def p_less_then_exp(p):
+    """
+        less_then_exp : less_then_exp LESS_THEN less_equals_exp
+                      | less_equals_exp
+    """
+
+
+def p_less_equals_exp(p):
+    """
+        less_equals_exp : less_equals_exp LESS_EQUALS left_shift_exp
+                        | left_shift_exp
+
+    """
+
+
+def p_left_shift_exp(p):
+    """
+        left_shift_exp : left_shift_exp BITWISE_SHIFT_LEFT right_shift_exp
+                        | right_shift_exp
+    """
+
+
+def p_right_shift_exp(p):
+    """
+        right_shift_exp : right_shift_exp BITWISE_SHIFT_RIGHT plus_exp
+                        | plus_exp
+    """
+
+
+def p_plus_exp(p):
+    """
+        plus_exp : plus_exp PLUS minus_exp
+                 | minus_exp
+    """
+
+
+def p_minus_exp(p):
+    """
+        minus_exp : minus_exp MINUS times_exp
+                  | times_exp
+    """
+
+
+def p_times_exp(p):
+    """
+        times_exp : times_exp TIMES divide_exp
+                  | divide_exp
+    """
+
+
+def p_divide_exp(p):
+    """
+        divide_exp : divide_exp DIVIDE modulus_exp
+                    | modulus_exp
+    """
+
+
+def p_modulus_exp(p):
+    """
+        modulus_exp : modulus_exp MODULUS unary_exp
+                    | unary_exp
+    """
+
+
+def p_unary_operator(p):
+    """
+        unary_operator : BITWISE_AND
+                       | TIMES
+                       | PLUS
+                       | MINUS
+                       | BITWISE_COMPLEMENT
+                       | NOT
+    """
+
+
+# Shift reduce
+def p_unary_exp(p):
+    """
+        unary_exp : prefix_exp
+    """
+
+
+def p_prefix_exp(p):
+    """
+        prefix_exp : postfix_exp
+              | INCREMENT unary_exp
+              | DECREMENT unary_exp
+              | unary_operator unary_exp
+              | cast_exp unary_exp
+              | sizeof_exp
+    """
+
+
+def p_sizeof_exp(p):
+    # shift/reduce
+    """
+                sizeof_exp : KEYWORD_SIZEOF unary_exp
+                            | KEYWORD_SIZEOF type
+                            | KEYWORD_SIZEOF pointer
+            """
+
+
+def p_cast_exp(p):
+    """
+        cast_exp : LPAREN type RPAREN
+                 | LPAREN pointer RPAREN
+    """
+
+
+def p_postfix_exp(p):
+    """
+        postfix_exp : postfix_exp LBRACKET expression RBRACKET
+                    | postfix_exp LPAREN RPAREN
+                    | postfix_exp LPAREN function_call_parameters RPAREN
+                    | postfix_exp DOT IDENTIFIER
+                    | postfix_exp ARROW IDENTIFIER
+                    | postfix_exp INCREMENT
+                    | postfix_exp DECREMENT
+                    | primary_exp
+    """
+
+
+def p_function_call_parameters(p):
+    """
+        function_call_parameters : expression
+                                  | expression COMMA function_call_parameters
+    """
+
+
+def p_primary_exp(p):
+    """
+        primary_exp : identifier_exp
+    """
+
+
+def p_identifier_exp(p):
+    """
+        identifier_exp : IDENTIFIER
+                        | string_exp
+    """
+
+
+def p_string_exp(p):
+    """
+        string_exp : STRING
+                    | number_exp
+    """
+
+
+def p_number_exp(p):
+    """
+        number_exp : integer_number
+                    | FLOAT_NUMBER
+                    | CHARACTER
+                    | parentesis_exp
+    """
+
+
+def p_parentesis_exp(p):
+    # shift reduce
+    """
+        parentesis_exp : LPAREN expression RPAREN
     """
 
 
